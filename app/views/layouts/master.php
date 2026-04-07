c640dc9ba45491f49c802ebb2222fd29ae2ea754
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-04-06 18:33:21 -0600

    (test) Added classless stylesheet link and bundle.min.js for dev working.

--- app/views/layouts/master.php
+++ app/views/layouts/master.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="/assets/css/app.css"> -->
+    <link rel="stylesheet" href="https://classless.de/classless.css">
     <title>Breif</title>
 </head>
 <body>
     <?= $content; ?>
+    <script src="/assets/js/bundle.min.js"></script>
 </body>
 </html>
